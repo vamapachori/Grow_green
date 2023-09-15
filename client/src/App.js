2e2683bfbe96b0abc212823cc8a105c93cbad4a1
vamapachori <107366048+vamapachori@users.noreply.github.com>
2023-09-15 16:12:35 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './pages/Services';
+import Products from './pages/Products';
+import ContactUs from './pages/ContactUs';
+import SignUp from './pages/SignUp';
+/* Include when available
+import Marketing from './pages/Marketing';
+import Consulting from './pages/Consulting'; */
+import Development from './pages/Development';
+import Design from './pages/Design';
+
+function App() {
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/services" component={Services} />
+				<Route path="/products" component={Products} />
+				<Route path="/contact-us" component={ContactUs} />
+				<Route path="/sign-up" component={SignUp} />
+				{/* <Route path="/marketing" component={Marketing} />
+				<Route path="/consulting" component={Consulting} /> */}
+				<Route path="/development" component={Development} />
+				<Route path="/design" component={Design} />
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
